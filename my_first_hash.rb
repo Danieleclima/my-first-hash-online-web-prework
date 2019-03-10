31b13cdca092dfcd9077778d304235f19fff9f8a
Daniel Lima <daniel.echuaca.lima@gmail.com>
2019-03-10 17:22:27 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -16,8 +16,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest ["oil paintings"]
 end
 
 def adding
@@ -27,7 +26,8 @@ def adding
                   "oil paintings" => 3
                  }
                  
-  shipping_manifest 
+  shipping_manifest[muskets] = 2
+  shipping_manifest [gun powder] = 4
   # add 2 muskets to the shipping_manifest hash below
 
 
